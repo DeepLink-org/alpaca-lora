3e577f15ad2eeaec23bdb78a4e8b94f234742384
liuyipeng <lzhaotimeless@163.com>
2024-02-01 17:49:10 +0800

    llama7b: modify requirements, only need accelerate & peft with assigned version

--- run_finetune.sh
+++
@@ -1,5 +0,0 @@
-WANDB_MODE=disabled srun -n1 -p pat_dev --gres=gpu:1 --tasks-per-node 1 --job-name=lora \
-python finetune.py \
---base_model '/mnt/lustre/share_data/PAT/datasets/llama-7b-hf/' \
---data_path 'alpaca_data.json' \
---output_dir './lora-alpaca' 
