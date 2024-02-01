15a0290513a93bbeea8b9b6583fc309a54952b22
liuyipeng <lzhaotimeless@163.com>
2024-02-01 21:01:35 +0800

    modify run finetune

--- run_finetune.sh
+++
@@ -1,5 +0,0 @@
-WANDB_MODE=disabled srun -n1 -p pat_dev --gres=gpu:1 --tasks-per-node 1 --job-name=lora \
-python finetune.py \
---base_model '/mnt/lustrenew/liuyipeng/Llama7B/mnt/lustre/share_data/diopi-sh/llama_hf/' \
---data_path 'alpaca_data.json' \
---output_dir './lora-alpaca' 
